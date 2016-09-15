8d5cb823edf7bdd91b07754ff7b8b994757e5f2d
songqingshan <songqingshan@ucredit.com>
2016-09-15 22:06:46 +0800

    更新sql语句

--- src/main/resources/sql/account.sql
+++ src/main/resources/sql/account.sql
@@ -1,18 +1,3 @@
-/*
-Navicat MySQL Data Transfer
-
-Source Server         : localhost
-Source Server Version : 50631
-Source Host           : localhost:3306
-Source Database       : nongzi
-
-Target Server Type    : MYSQL
-Target Server Version : 50631
-File Encoding         : 65001
-
-Date: 2016-08-18 23:33:37
-*/
-
 SET FOREIGN_KEY_CHECKS=0;
 
 -- ----------------------------
@@ -22,10 +7,13 @@ DROP TABLE IF EXISTS `account`;
 CREATE TABLE `account` (
   `account_id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(32) NOT NULL COMMENT '用户名',
+  `customer_id` int(11) DEFAULT NULL COMMENT '对应的客户id',
   `password` varchar(64) NOT NULL COMMENT '用户密码',
   `type` int(4) DEFAULT '0' COMMENT '用户类别(0:普通用户,1:管理员用户)',
-  `create_time` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
-  `update_time` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
+  `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `update_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `enable` tinyint(1) DEFAULT NULL,
-  PRIMARY KEY (`account_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COMMENT='用户登录表';
+  PRIMARY KEY (`account_id`),
+  KEY `idx_customer_id` (`customer_id`) USING BTREE,
+  KEY `idx_name` (`name`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='用户登录表';
--- src/main/resources/sql/sale.sql
+++ src/main/resources/sql/sale.sql
@@ -1,18 +1,3 @@
-/*
-Navicat MySQL Data Transfer
-
-Source Server         : localhost
-Source Server Version : 50631
-Source Host           : localhost:3306
-Source Database       : nongzi
-
-Target Server Type    : MYSQL
-Target Server Version : 50631
-File Encoding         : 65001
-
-Date: 2016-08-18 23:34:05
-*/
-
 SET FOREIGN_KEY_CHECKS=0;
 
 -- ----------------------------
@@ -29,8 +14,8 @@ CREATE TABLE `sale` (
   `num` int(11) DEFAULT NULL COMMENT '销售数量',
   `total_price` decimal(10,2) DEFAULT NULL COMMENT '销售总价(单位 元)',
   `profit` decimal(10,2) DEFAULT NULL COMMENT '盈利(单位 元)',
-  `create_time` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
-  `update_time` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
+  `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `update_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `enable` tinyint(3) unsigned DEFAULT NULL COMMENT '是否有效(0:无效,1:有效)',
   PRIMARY KEY (`sale_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COMMENT='销售表';
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='销售表';
